a9534dee7736ce509ad2c196ee84c094333c9468
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-08-22 12:53:10 -0300

    fix: Correção na sintaxe da importação do Header

--- modulo-2-ignitetimer/src/layouts/DefaultLayout.tsx
+++ modulo-2-ignitetimer/src/layouts/DefaultLayout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../componentes/Header'
+import { Header } from '../components/Header'
 
-export function DefaultLayout(){
-    return(
-        <div>
-            <Header />
-            <Outlet />
-        </div>
-    );
+export function DefaultLayout() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  )
 }
