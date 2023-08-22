ab0b67f58f50b9fded1467a6fcea31ac8d347192
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-08-22 12:53:43 -0300

    fix: Retirada do Header importado direto

--- modulo-2-ignitetimer/src/pages/History.tsx
+++ modulo-2-ignitetimer/src/pages/History.tsx
@@ -1,10 +1,3 @@
-import { Header } from '../components/Header'
-
 export function History() {
-  return(
-    <div>
-      <Header/>
-      <h1>History</h1>
-    </div>
-  );
+  return <h1>History</h1>
 }
--- modulo-2-ignitetimer/src/pages/Home.tsx
+++ modulo-2-ignitetimer/src/pages/Home.tsx
@@ -1,10 +1,3 @@
-import { Header } from '../components/Header'
-
 export function Home() {
-  return (
-    <div>
-      <Header />
-      <h1>Home</h1>
-    </div>
-  )
+  return <h1>Home</h1>
 }
