f4138e34760caa656f7a1629e03f6a7f6b258308
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-08-22 12:25:43 -0300

    fix: Retirada do Button e add a Route

--- modulo-2-ignitetimer/src/App.tsx
+++ modulo-2-ignitetimer/src/App.tsx
@@ -2,15 +2,14 @@ import { Button } from './components/Button.tsx'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.ts'
 import { GlobalStyle } from './styles/global.ts'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './components/Router.tsx'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
