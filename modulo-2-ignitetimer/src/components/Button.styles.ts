638ecfdf97f594d67279772a1de867b33621550c
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-08-22 11:43:37 -0300

    fix: Retirada do componente button de exemplo

--- modulo-2-ignitetimer/src/components/Button.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const ButtonContainer = styled.button<ButtonContainerProps>`
-  width: 100px;
-  height: 40px;
-
-  background-color: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme.white};
-`
