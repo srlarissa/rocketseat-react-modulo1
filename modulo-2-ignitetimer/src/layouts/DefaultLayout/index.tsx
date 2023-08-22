70c71609ce9a1ef19aa775a21c207e4a9904f5eb
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-08-22 13:09:45 -0300

    feat(defaultlayout): Aplicação do style

--- modulo-2-ignitetimer/src/layouts/DefaultLayout/index.tsx
+++ modulo-2-ignitetimer/src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
+import { LayoutContainer } from './style'
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
