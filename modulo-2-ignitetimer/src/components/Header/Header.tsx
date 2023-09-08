d2e7151c48273e0f05f877e85ad163509fd5c129
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-09-08 16:47:58 -0300

    fix: Correção do componente Header

---
+++ modulo-2-ignitetimer/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import { HeaderContainer } from "./styles";
+
+export function Header() {
+  return(
+    <HeaderContainer>
+      <span>Logo</span>
+      <nav>
+        <a href="#">Timer</a>
+        <a href="#">History</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
+ 
