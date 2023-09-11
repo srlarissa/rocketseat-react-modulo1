7f6224fa1b4cfc8cce5eab5e92d80341c221c260
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-09-11 12:11:49 -0300

    feat: Add componentes styled separadas para o projeto e minutos
    Criação de um input base compartilhado por ambos inputs e criação dos inputs separados baseado no input base

--- modulo-2-ignitetimer/src/pages/Home/styles.ts
+++ modulo-2-ignitetimer/src/pages/Home/styles.ts
@@ -27,6 +27,34 @@ export const FormContainer = styled.div`
     flex-wrap: wrap;
 `
 
+export const BaseInput = styled.input`
+    background-color: transparent;
+    height: 2.5rem;
+    border: 0;
+    border-bottom: 2px solid ${props => props.theme['gray-500']};
+    font-weight: bold;
+    font-size: 1.125rem;
+    padding: 0 0.5rem;
+    color: ${props => props.theme['gray-100']};
+
+    &::placeholder{
+        color: ${props => props.theme['gray-500']};
+    }
+
+    &:focus{
+        box-shadow: none;
+        border-color: ${props => props.theme['green-500']};
+    }
+`
+
+export const TaskInput = styled(BaseInput)`
+    flex: 1;
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+    width: 4rem
+`
+
 export const CountDownContainer = styled.div`
     font-family: 'Roboto Mono', monospace;
     font-size: 10rem;
@@ -72,7 +100,7 @@ export const StartCountDownButton = styled.button`
     background: ${props => props.theme['green-500']};
     color: ${props => props.theme['gray-100']};
 
-    &:hover{
+    &:not(:disabled):hover{
         background-color: ${props => props.theme['green-700']};
     }
 
