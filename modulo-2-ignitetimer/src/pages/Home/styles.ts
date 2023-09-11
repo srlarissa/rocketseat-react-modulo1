6048f04199d302472bd328733f574fab783d9b8c
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-09-11 11:28:20 -0300

    feat: Add style home, numeros countdown e form

--- modulo-2-ignitetimer/src/pages/Home/styles.ts
+++ modulo-2-ignitetimer/src/pages/Home/styles.ts
@@ -28,5 +28,18 @@ export const FormContainer = styled.div`
 `
 
 export const CountDownContainer = styled.div`
-    
+    font-family: 'Roboto Mono', monospace;
+    font-size: 10rem;
+    line-height: 8rem;
+    color: ${props => props.theme['gray-100']};
+
+    display: flex;
+    gap: 1rem;
+
+    span{
+        background-color: ${props => props.theme['gray-700']};
+        padding: 2rem 1rem;
+        border-radius: 8px;
+    }
+
 `
