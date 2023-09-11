511b354cf9190ebb561230ee5a319b1281dcfd39
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-09-11 18:14:25 -0300

    feat: Add state que armazena o id do ciclo ativo

--- modulo-2-ignitetimer/src/pages/Home/index.tsx
+++ modulo-2-ignitetimer/src/pages/Home/index.tsx
@@ -26,11 +26,12 @@ type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 interface Cycle {
   id: string,
   task: string,
-  minutesAmount: number
+  minutesAmount: number,
 }
 
 export function Home() {
-  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [ cycles, setCycles ] = useState<Cycle[]>([])
+  const [ activeCycleId, setActiveCycleId ] = useState<string | null>(null)
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -41,16 +42,21 @@ export function Home() {
   })
 
   function handleCreateNewCycle(data: NewCycleFormData) {
+    const id = String(new Date().getTime());
     const newCycle: Cycle = {
-      id: String(new Date().getTime()),
+      id,
       task: data.task,
       minutesAmount: data.minutesAmount,
     }
 
     setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
 
     reset()
   }
+
+  const activeCycle = cycles.find(cycle => cycle.id == activeCycleId)
+  console.log(activeCycle)
 
   const task = watch('task')
   const isSubmitDisabled = !task
