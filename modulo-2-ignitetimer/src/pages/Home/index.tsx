4585be30a471d16e572c3bb5b4242e6ef65d9e68
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-09-11 14:41:42 -0300

    feat: Implementação disabled botão Começar
    Caso a task esteja vazia, o botão estará desabilitado

--- modulo-2-ignitetimer/src/pages/Home/index.tsx
+++ modulo-2-ignitetimer/src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
 import { Play } from "phosphor-react";
 import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountDownButton, TaskInput } from "./styles";
+import { useState } from "react";
 
 export function Home() {
+  const [task, setTask] = useState('');
+  
   return(
     <HomeContainer>
       <form action="">
@@ -10,7 +13,9 @@ export function Home() {
           <TaskInput 
             type="task" 
             placeholder="Dê um nome para o seu projeto" 
-            list="task-suggestions"  
+            list="task-suggestions" 
+            onChange={(e) => setTask(e.target.value)}
+            value={task} 
           />
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
@@ -38,7 +43,7 @@ export function Home() {
             <span>0</span>
             <span>0</span>
           </CountDownContainer>
-          <StartCountDownButton disabled type="submit">
+          <StartCountDownButton disabled={!task} type="submit">
             <Play size={24}/>
             Começar
           </StartCountDownButton>
