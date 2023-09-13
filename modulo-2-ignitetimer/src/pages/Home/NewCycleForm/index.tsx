55ef1c45ed1a8240794dfa319e921e8861f4b697
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-09-13 17:31:30 -0300

    style: Dissolução do Novo Ciclo em um componente

---
+++ modulo-2-ignitetimer/src/pages/Home/NewCycleForm/index.tsx
@@ -0,0 +1,36 @@
+import { FormContainer, TaskInput, MinutesAmountInput } from "./styles";
+
+export function NewCycleForm(){
+    return(
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+            disabled={!!activeCycle}
+            {...register('task')}
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            disabled={!!activeCycle}
+            placeholder="00"
+            step={5}
+            min={1}
+            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+    )
+}
--- modulo-2-ignitetimer/src/pages/Home/index.tsx
+++ modulo-2-ignitetimer/src/pages/Home/index.tsx
@@ -134,36 +134,7 @@ export function Home() {
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
-        <FormContainer>
-          <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput
-            type="task"
-            placeholder="Dê um nome para o seu projeto"
-            list="task-suggestions"
-            disabled={!!activeCycle}
-            {...register('task')}
-          />
-          <datalist id="task-suggestions">
-            <option value="Projeto 1" />
-            <option value="Projeto 2" />
-            <option value="Projeto 3" />
-            <option value="Projeto 4" />
-          </datalist>
-
-          <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput
-            type="number"
-            id="minutesAmount"
-            disabled={!!activeCycle}
-            placeholder="00"
-            step={5}
-            min={1}
-            max={60}
-            {...register('minutesAmount', { valueAsNumber: true })}
-          />
-
-          <span>minutos.</span>
-        </FormContainer>
+        
         <CountDownContainer>
           <span>{minutes[0]}</span>
           <span>{minutes[1]}</span>
